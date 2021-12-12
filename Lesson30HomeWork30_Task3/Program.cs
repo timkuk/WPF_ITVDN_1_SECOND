93bc7f8e6de93b1b260bdf67c3d73806cc0b5d30
=Kurchenok Tim <tim_fedorenko@mail.ru>
2021-12-12 23:59:47 +0300

    fix:Lesso30 Task 1-3

--- Lesson30HomeWork30_Task3/Program.cs
+++ Lesson30HomeWork30_Task3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System;
 using System.Text;
+using System.IO.Compression;
 //Напишите приложение для поиска заданного файла на диске
 //Добавьте код, использующий класс FileStream и позволяющий просматривать файл в текстовом окне
 //В заключение добавьте возможность сжатия найденного файла
@@ -10,22 +11,37 @@ namespace Lesson30HomeWork30_Task3
     {
         static void Main(string[] args)
         {
-            string[] drives = Directory.GetLogicalDrives();
-            Console.WriteLine("Имеющиеся диски:");
-
-            foreach (string drive in drives)
-                Console.WriteLine("- {0}", drive);
-            Console.WriteLine("Select disk ");
-            //ReadAllText();
+            var searchFile = new FileInfo(@"E:\Test.txt");
+            Console.WriteLine("Поиск заданного файла на диске");
+            try
+            {
+                FileStream stream = searchFile.OpenRead();
+                Console.WriteLine($"Заданный файл найден {searchFile}");
+                FileStream source = File.Create(@"E:\Test.zip");
+                Console.WriteLine("Архивирование исходного файла");
+                GZipStream compressor = new GZipStream(source, CompressionMode.Compress);
+                int TheByte = stream.ReadByte();
+                while(TheByte != -1)
+                {
+                    compressor.WriteByte((byte)TheByte);
+                    TheByte = stream.ReadByte();
+                }
+                Console.WriteLine("\nТекст из файлика в консольном окне:");
+                StreamReader reader = File.OpenText(@"E:\Test.txt");
+                string input;
+                // Выводим содержимое файла в консоль.
+                while ((input = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(input);
+                }
+                compressor.Close();
+                reader.Close();
+                stream.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Такого файла не существует");
+            }
         }
-        //public static string ReadAllText(string filePath)
-        //{
-        //    using (FileStream stream = File.OpenRead(filePath))
-        //    {
-        //        var encoding = new UTF8Encoding(true);
-        //        var reader = new StreamReader(stream, encoding);
-        //        return reader.ReadToEnd();
-        //    }
-        //}
     }
 }
