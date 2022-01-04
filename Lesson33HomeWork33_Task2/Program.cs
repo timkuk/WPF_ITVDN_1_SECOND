87c60d6ff9f2d31879c928929eb6eb7b6e0fbd35
=Kurchenok Tim <tim_fedorenko@mail.ru>
2022-01-04 17:50:08 +0300

    fix:Debug

---
+++ Lesson33HomeWork33_Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson33HomeWork33_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Lesson33HomeWork33_Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson33HomeWork33_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
