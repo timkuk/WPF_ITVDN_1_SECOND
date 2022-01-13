43377399b3a78184354c330bd1ebc753612a52c6
=Kurchenok Tim <tim_fedorenko@mail.ru>
2022-01-13 17:50:56 +0300

    fix:

---
+++ Lesson35HomeWork35_Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+//Создайте новое приложение, в котором выполните десериализацию объекта из предыдущего примера. Отобразите состояние объекта на экране
+namespace Lesson35HomeWork35_Task3
+{
+    public class  Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
