c9b4852e1d0145dc19cb24326cbac43f05bf4ad3
=Kurchenok Tim <tim_fedorenko@mail.ru>
2022-01-17 22:31:33 +0300

    fix:

---
+++ Lesson36HomeWork36_Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Lesson36HomeWork36_Task1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Lesson36HomeWork36_Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson36HomeWork36_Task2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
